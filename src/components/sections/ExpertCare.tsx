6b7471ab2004f1b32bd2b93c8c9a9989c2d8d51f
ashjal uk <ashjalribu811@gmail.com>
2026-05-06 18:21:44 +0400

    updated maternity  website

--- src/components/sections/ExpertCare.tsx
+++ src/components/sections/ExpertCare.tsx
@@ -20,10 +20,10 @@ export const ExpertCare = () => {
                 alt="Lead Doctor"
                 className="w-full h-full object-cover"
               />
-              <div className="absolute bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-black/80 to-transparent text-white">
+              <div className="absolute bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-black/80 to-transparent text-white text-center">
                 <h3 className="text-3xl text-white/90 font-serif font-bold">Dr. Aparna Nath K</h3>
                 <p className="text-white/90 font-medium">BAMS, MS (Ay)</p>
-                <p className="text-white/90 font-small">Specialist in Ayurveda Gynaecology and Women’s Health</p>
+                <p className="text-white/70 font-small">Specialist in Ayurveda Gynaecology and Women’s Health</p>
               </div>
             </div>
             {/* Decoration */}
