176facb801fd65d7b069647f9efa73b328fd7dce
ashjal uk <ashjalribu811@gmail.com>
2026-07-31 00:04:28 +0400

    Update website

--- src/components/sections/CenterGallery.tsx
+++ src/components/sections/CenterGallery.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { SectionHeading } from "../ui/SectionHeading";
-import img1 from "../../assets/gallery/1.jpeg";
+// import img1 from "../../assets/gallery/1.jpeg";
 import img2 from "../../assets/gallery/2.jpeg";
-import img3 from "../../assets/gallery/3.jpeg";
+// import img3 from "../../assets/gallery/3.jpeg";
 import img4 from "../../assets/gallery/4.jpeg";
-import img5 from "../../assets/gallery/5.jpeg";
+// import img5 from "../../assets/gallery/5.jpeg";
 import img6 from "../../assets/gallery/6.jpeg";
-import img7 from "../../assets/gallery/7.jpeg";
+// import img7 from "../../assets/gallery/7.jpeg";
 import img8 from "../../assets/gallery/8.jpeg";
-import img9 from "../../assets/gallery/9.jpeg";
+// import img9 from "../../assets/gallery/9.jpeg";
 import img10 from "../../assets/gallery/10.jpeg";
-import img11 from "../../assets/gallery/11.jpeg";
-import img12 from "../../assets/gallery/12.jpeg";
-import img13 from "../../assets/gallery/13.jpeg";
+// import img11 from "../../assets/gallery/11.jpeg";
+// import img12 from "../../assets/gallery/12.jpeg";
+// import img13 from "../../assets/gallery/13.jpeg";
 import img14 from "../../assets/gallery/14.jpeg";
-import img15 from "../../assets/gallery/15.jpeg";
+// import img15 from "../../assets/gallery/15.jpeg";
 
 const IMAGES = [
   /*
