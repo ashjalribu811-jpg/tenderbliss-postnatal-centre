348c1c16c68f716d51e2c907cefe545beffc584c
ashjal uk <ashjalribu811@gmail.com>
2026-07-30 23:39:07 +0400

    Update website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import { Navbar } from './components/layout/Navbar';
 import { Footer } from './components/layout/Footer';
